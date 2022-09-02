da6e90ab1f30bba42898588af5de1550f521492b
Ali <aliagayev132@gmail.com>
2022-09-02 05:28:44 +0400

    V1.1 | Added toggle menu,Some Responsive designs,fixin css problems |

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
-const section = {
+const toggleBtn = document.querySelector('#toggle__btn'),
+navbar__items = document.querySelector(".navbar__items") ,
+section = {
     home:document.querySelector('.section__home'),
     gallery:document.querySelector('.section__gallery'),
     about:document.querySelector('.section__about'),
@@ -11,7 +13,9 @@ document.addEventListener('click',(e)=>{
         updatePage(id);
     }
 })
-
+toggleBtn.addEventListener('click',(e)=>{
+    navbar__items.classList.toggle("position__abs__menu");
+});
 function updatePage(page){
     section[currentid].classList.remove("display--block");
     section[page].classList.add("display--block");
