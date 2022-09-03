39c3229bdc93e396b71e5c3c645f555b7b03806f
Ali <aliagayev132@gmail.com>
2022-09-03 14:55:29 +0400

    V1.2 |Home,About and Contact's sections responsive problems fixed|

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,33 @@
-const toggleBtn = document.querySelector('#toggle__btn'),
-navbar__items = document.querySelector(".navbar__items") ,
-section = {
-    home:document.querySelector('.section__home'),
-    gallery:document.querySelector('.section__gallery'),
-    about:document.querySelector('.section__about'),
-    contact:document.querySelector('.section__contact')
-};
+const toggleBtn = document.querySelector("#toggle__btn"),
+  navbar__items = document.querySelector(".navbar__items"),
+  section = {
+    home: document.querySelector(".section__home"),
+    gallery: document.querySelector(".section__gallery"),
+    about: document.querySelector(".section__about"),
+    contact: document.querySelector(".section__contact"),
+  };
 let currentid = "home";
-document.addEventListener('click',(e)=>{
-    const id = e.target.getAttribute("id");
-    if((id === "home" || id === "gallery" || id === "about" || id === "contact" ) && currentid !== id){
-        updatePage(id);
-    }
-})
-toggleBtn.addEventListener('click',(e)=>{
-    navbar__items.classList.toggle("position__abs__menu");
+document.addEventListener("click", (e) => {
+  const id = e.target.getAttribute("id");
+  if (
+    (id === "home" || id === "gallery" || id === "about" || id === "contact") &&
+    currentid !== id
+  ) {
+    updatePage(id);
+  }
 });
-function updatePage(page){
-    section[currentid].classList.remove("display--block");
-    section[page].classList.add("display--block");
-    currentid = page;
+toggleBtn.addEventListener("click", (e) => {
+  navbar__items.classList.toggle("position__abs__menu");
+});
+function updatePage(page) {
+  section[currentid].classList.remove("display--block");
+  section[page].classList.add("display--block");
+  let lastItem = document.querySelector("#" + currentid),
+    currentItem = document.querySelector("#" + page);
+  lastItem.classList.remove("color--green");
+  currentItem.classList.add("color--green");
+  lastItem.nextElementSibling.classList.remove("bg_color--green");
+  currentItem.nextElementSibling.classList.add("bg_color--green");
+
+  currentid = page;
 }
-
-
-
