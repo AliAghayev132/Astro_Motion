71941637500d9e3435ba28f07dce04a48002c91e
Ali <aliagayev132@gmail.com>
2022-09-06 16:11:55 +0400

    V1.6 |CSS&JAVASCRIPT:Gallery Section changed (flex ==> grid);Script optimized;  |

--- src/js/main.js
+++ src/js/main.js
@@ -10,33 +10,43 @@ function classRemove(par, class_name) {
 }
 //#endregion
 // #region Initializing Sections and Variables
-const toggleBtn = qS("#toggle__btn"),
-  navbar__items = qS(".navbar__items"),
-  section = {
-    home: qS(".section__home"),
-    gallery: qS(".section__gallery"),
-    about: qS(".section__about"),
-    contact: qS(".section__contact")
-  };
+const section = {
+  home: qS(".section__home"),
+  gallery: qS(".section__gallery"),
+  about: qS(".section__about"),
+  contact: qS(".section__contact")
+},
+SIZES = {
+  cards:[5, 4, 3, 2, 1],
+  screens:[1200, 800, 600, 400, 300],
+  margin:25,
+};
 let currentid = "home";
 // #endregion
 // #region Adding Events
-document.addEventListener("click", (e) => {
-  let id = e.target.getAttribute("id");
-  id = (id === "gallery--2" ? "gallery" : id);
-  if (
-    (id === "home" || id === "gallery" || id === "about" || id === "contact") &&
-    currentid !== id
-  )
-    updatePage(id);
-
-});
-toggleBtn.addEventListener("click", (e) => {
-  navbar__items.classList.toggle("position__abs__menu");
-});
+function addEventListeners() {
+  document.addEventListener("click", (e) => {
+    let id = e.target.getAttribute('id');
+    if (e.target.classList.contains("nav__item")) {
+      updatePage(id);
+    } else if (id === "toggle__btn") {
+      qS(".navbar__items").classList.toggle("position__abs__menu");
+    } else if (id === "arrow__left") {
+      cardMove(-1);
+    } else if (id === "arrow__right") {
+      cardMove(1);
+    }
+  });
+  window.addEventListener("resize", () => {
+    console.log("Salam");
+    updateCarousel(SIZES.cards,SIZES.screens,SIZES.margin);
+  })
+}
 //#endregion
 // #region UpdatePage
 function updatePage(page) {
+  page = (page === "gallery--2" ? "gallery" : page);
+
   let lastItem = qS("#" + currentid),
     currentItem = qS("#" + page);
 
@@ -44,66 +54,50 @@ function updatePage(page) {
   classAdd(section[page], "display--block");
 
   if (page === "gallery")
-    updateGallery();
+    updateCarousel(SIZES.cards,SIZES.screens,SIZES.margin);
 
   currentid = page;
 
+  classRemove(lastItem.children[0], "color--green");
+  classAdd(currentItem.children[0], "color--green");
 
-  classRemove(lastItem, "color--green");
-  classAdd(currentItem, "color--green");
-
-  classRemove(lastItem.nextElementSibling, "bg_color--green");
-  classAdd(currentItem.nextElementSibling, "bg_color--green");
+  classRemove(lastItem.children[1], "bg_color--green");
+  classAdd(currentItem.children[1], "bg_color--green");
 
 }
 // #endregion
 
 
+//!Sonrada Marginidə qoş avto
 //#region Carousel 
-function updateGallery() {
-  let currentCard = 5;
-  const content = qS(".gallery__content"),
-    main = qS(".gallery__main"),
-    left_arrow = qS('.gallery__arrow--left'),
-    right_arrow = qS('.gallery__arrow--right'),
-    cards = content.children;
-
-  let margin_right = 25,
-    width = main.getBoundingClientRect().width,
-    card_width = (width - margin_right * 4) / 5;
-
-  for (let i of cards)
-    i.style.width = card_width + 'px';
-
-  content.style.width = (cards.length * card_width) + (margin_right * cards.length) + 'px';
-  window.addEventListener('resize', () => {
-    let width = main.getBoundingClientRect().width;
-    let card_width = (width - (margin_right * 4)) / 5;
-    for (let i of cards) {
-      i.style.width = card_width + 'px';
+function updateCarousel(visible_cards, widths, margin) {
+  const main = document.querySelector('.gallery__main').getBoundingClientRect().width,
+  content = document.querySelector('.gallery__content');
+  for(let i in visible_cards){
+    if(main > widths[i]){
+      content.style.setProperty("--per-view",visible_cards[i]);
+      break;
     }
-  })
-
-  right_arrow.addEventListener('click', () => {
-    currentCard += 3;
-    console.log(currentCard);
-    if (currentCard > cards.length) {
-      currentCard = cards.length;
-    }
-
-    let temp = (card_width * (currentCard - 5)) + (margin_right * (currentCard - 6));
-    content.style.transform = `translateX(${-temp}px)`;
-  })
-  left_arrow.addEventListener('click', () => {
-    currentCard -= 3;
-    console.log(currentCard)
-    if (currentCard < 5) {
-      currentCard = 5;
-    }
-    console.log(currentCard)
-    let temp = (card_width * (currentCard - 5)) + (margin_right * (currentCard - 5));
-    content.style.transform = `translateX(${-temp}px)`;
-  })
-
+  }
 }
 //#endregion
+function cardMove(par) {
+  let nextposition;
+  if (par === -1) {
+    startCard -= 3;
+    if (startCard < 0) {
+      startCard = 0;
+      nextposition = (startCard * card_width);
+      content.style.transform = `translateX(${-nextposition}px)`;
+    } else {
+      nextposition = (startCard * card_width) + (startCard) * card_margin;
+      content.style.transform = `translateX(${-nextposition}px)`;
+    }
+  } else {
+    let temp = cards.length - visible_card;
+    startCard = startCard + 3 > temp ? temp : startCard + 3;
+    nextposition = (startCard * card_width) + (startCard - 1) * card_margin;
+    content.style.transform = `translateX(${-nextposition}px)`;
+  }
+}
+addEventListeners();
